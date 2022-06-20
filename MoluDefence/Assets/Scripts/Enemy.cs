ff91be68c7a46426c4ec945e52e9c679c5ad9d62
Adle0na <uuuuufgh@gmail.com>
2022-06-21 03:44:40 +0900

    중간 저장

--- MoluDefence/Assets/Scripts/Enemy.cs
+++ MoluDefence/Assets/Scripts/Enemy.cs
@@ -2,12 +2,64 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Enemy : MonoBehaviour
 {
-    private int         _wayPointCount;
-    private Transform[] _wayPoints;
-    private int         _currentIndex = 0;
-    private Movement2D  _movement2D;
-    
-    public void Setup(Transform[] wayPoints)
+    private int          _wayPointCount;
+    private Transform[]  _wayPoints;
+    private int          _currentIndex = 0;
+    private Movement2D   _movement2D;
+    private EnemySpawner _enemySpawner;
+
+    public void Setup(EnemySpawner enemySpawner, Transform[] wayPoints)
+    {
+        _movement2D        = GetComponent<Movement2D>();
+        this._enemySpawner = enemySpawner;
+
+        _wayPointCount     = wayPoints.Length;
+        this._wayPoints    = new Transform[_wayPointCount];
+        this._wayPoints    = wayPoints;
+
+        transform.position = _wayPoints[_currentIndex].position;
+
+        StartCoroutine("OnMove");
+    }
+
+    private IEnumerator OnMove()
+    {
+        NextMoveTo();
+
+        while (true)
+        {
+            transform.Rotate(Vector3.forward * 10);
+
+            if (Vector3.Distance(transform.position, _wayPoints[_currentIndex].position) < 0.02f * _movement2D.MoveSpeed)
+            {
+                NextMoveTo();
+            }
+
+            yield return null;
+        }
+        
+    }
+
+    private void NextMoveTo()
+    {
+        if (_currentIndex < _wayPointCount - 1)
+        {
+            transform.position = _wayPoints[_currentIndex].position;
+            _currentIndex++;
+            Vector3 direction = (_wayPoints[_currentIndex].position - transform.position).normalized;
+            _movement2D.MoveTo(direction);
+        }
+        else
+        {
+            // Destroy(gameObject);
+            OnDie();
+        }
+    }
+
+    public void OnDie()
+    {
+        _enemySpawner.DestroyEnemy(this);
+    }
 }
